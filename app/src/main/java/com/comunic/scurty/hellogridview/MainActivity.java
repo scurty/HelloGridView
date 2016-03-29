c78ecc1b7b575fb9b6141f45eba8da5433b1ca49
scurty <scurtycel@gmail.com>
2016-03-29 16:53:36 -0300

    asdfdf

---
+++ app/src/main/java/com/comunic/scurty/hellogridview/MainActivity.java
@@ -0,0 +1,91 @@
+package com.comunic.scurty.hellogridview;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.Toast;
+
+import com.cardiomood.android.controls.gauge.SpeedometerGauge;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            SpeedometerGauge speedometer;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                speedometer = new SpeedometerGauge(mContext);
+              //  speedometer.setLayoutParams(new GridView.LayoutParams(85, 85));
+                // speedometer.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                   speedometer.setPadding(8, 8, 8, 8);
+
+            } else {
+                speedometer = (SpeedometerGauge) convertView;
+            }
+
+           // speedometer.setBackground(getResources().getDrawable(mThumbIds[position]));
+           // speedometer.setImageResource(mThumbIds[position]);
+            return speedometer;
+        }
+
+        // references to our images
+        private Integer[] mThumbIds = {
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7,
+                R.drawable.sample_0, R.drawable.sample_1,
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7,
+                R.drawable.sample_0, R.drawable.sample_1,
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7
+        };
+    }
+}
